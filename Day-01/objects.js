243f603b90483e91e4ed9f5972d62e44c29d294e
balaram <balaramreddy1710@gmail.com>
2025-05-06 10:40:43 +0530

    [Balaram] Add: Objects in typescript

---
+++ Day-01/objects.js
@@ -0,0 +1,10 @@
+var car = {
+    type: "toyota",
+    name: "etios",
+};
+console.log(car);
+var car1 = {
+    type: "ford",
+};
+// car1.type = 23;
+console.log(car1);
---
+++ Day-01/objects.ts
@@ -0,0 +1,13 @@
+const car: { type: string; name: string } = {
+  type: "toyota",
+  name: "etios",
+};
+
+console.log(car);
+
+const car1 = {
+  type: "ford",
+};
+
+// car1.type = 23;
+console.log(car1);
