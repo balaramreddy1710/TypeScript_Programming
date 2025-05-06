e6740d95c7fa07ab64d09ee3a565556c0fd4a1a3
balaram <balaramreddy1710@gmail.com>
2025-05-06 11:22:17 +0530

    [Balaram] Add: String enum in typescript

---
+++ Day-02/enumStr.ts
@@ -0,0 +1,9 @@
+enum CardinalDirections {
+  North = "North",
+  East = "East",
+  South = "South",
+  West = "West",
+}
+console.log(CardinalDirections.North);
+
+console.log(CardinalDirections.West);
