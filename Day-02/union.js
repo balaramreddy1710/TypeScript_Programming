cbe7ae4e72bf872e6ad1eabf21d9525e03e172f6
balaram <balaramreddy1710@gmail.com>
2025-05-06 12:17:13 +0530

    [Balaram] Add: Union of types in typescript

---
+++ Day-02/union.js
@@ -0,0 +1,6 @@
+function printStatusCode(code) {
+    console.log("My status code is ".concat(code, "."));
+    //   console.log(`My status code is ${code.toUpperCase()}.`);
+}
+printStatusCode(404);
+printStatusCode("404");
---
+++ Day-02/union.ts
@@ -0,0 +1,6 @@
+function printStatusCode(code: string | number) {
+  console.log(`My status code is ${code}.`);
+  //   console.log(`My status code is ${code.toUpperCase()}.`);
+}
+printStatusCode(404);
+printStatusCode("404");
