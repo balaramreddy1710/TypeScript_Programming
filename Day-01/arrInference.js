a7a5942eafb449e0688f1ce4c7f28ed1e80ae233
balaram <balaramreddy1710@gmail.com>
2025-05-06 10:39:25 +0530

    [Balaram] Add: Inferring data type of array in typescript

---
+++ Day-01/arrInference.js
@@ -0,0 +1,6 @@
+var numbers = [1, 2, 3];
+numbers.push(4);
+console.log(numbers);
+// numbers.push("2");
+var head = numbers[0];
+console.log(head);
---
+++ Day-01/arrInference.ts
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3];
+numbers.push(4);
+console.log(numbers);
+
+// numbers.push("2");
+
+let head: number = numbers[0];
+console.log(head);
