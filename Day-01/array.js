a43527f35a00a190c0ade879c25957ecb0ca3fad
balaram <balaramreddy1710@gmail.com>
2025-05-06 10:37:57 +0530

    [Balaram] Add: Arrays in typescript

---
+++ Day-01/array.js
@@ -0,0 +1,9 @@
+var arr = ["name", "age"];
+arr.push("class");
+console.log(arr);
+var arr1 = [1, 2, 3];
+arr1.push(4);
+console.log(arr1);
+var arr2 = ["name", "age"];
+// arr2.push("class");
+console.log(arr2);
---
+++ Day-01/array.ts
@@ -0,0 +1,17 @@
+let arr: string[] = ["name", "age"];
+
+arr.push("class");
+
+console.log(arr);
+
+let arr1 = [1, 2, 3];
+
+arr1.push(4);
+
+console.log(arr1);
+
+let arr2: readonly string[] = ["name", "age"];
+
+// arr2.push("class");
+
+console.log(arr2);
