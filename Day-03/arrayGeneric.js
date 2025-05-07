694d5141c2541802896cd796c66a14a93edf976a
balaram <balaramreddy1710@gmail.com>
2025-05-07 10:59:08 +0530

    [Balaram] Add: Using generics in arrays in typescript

---
+++ Day-03/arrayGeneric.js
@@ -0,0 +1,5 @@
+function first(arr) {
+    return arr;
+}
+var result = first([1, 2, 3]);
+console.log(result);
---
+++ Day-03/arrayGeneric.ts
@@ -0,0 +1,6 @@
+function first<T>(arr: T[]): T[] {
+  return arr;
+}
+
+const result = first([1, 2, 3]);
+console.log(result);
