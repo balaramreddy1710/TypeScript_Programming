f1fdfca35a13eb86a0eeee1ed4497470b8c57e72
balaram <balaramreddy1710@gmail.com>
2025-05-08 11:36:50 +0530

    [Balaram] Add: Exception handling with finally in typescript

---
+++ Day-04/finally.js
@@ -0,0 +1,16 @@
+try {
+    function errorfunc(num1, num2) {
+        if (typeof num1 == "number" && typeof num2 == "number") {
+            console.log(num1 + num2);
+        }
+        throw new ReferenceError("Parameter/s are not type of numbers.");
+    }
+    errorfunc("11", 12);
+}
+catch (error) {
+    console.log("Inside the catch block.");
+    console.log("The error is " + error);
+}
+finally {
+    console.log("Inside the finally block to execute the code before program termination.");
+}
